39ed93bb3cd3d1a09111f57451521dcae57d04b1
sebastianosalzillo <salzillosebastiano@gmail.com>
2025-03-21 15:54:21 +0100

    feat : milestone 2 completato

--- src/GetUser.jsx
+++ src/GetUser.jsx
@@ -13,22 +13,53 @@ function GetUser() {
         }
         fetchData()
     }, []);
+
+
+
+    const [valore, setValore] = useState("")
+
+    function handleChange(e) {
+        setValore(e.target.value)
+    }
+
+    const filtrati = user.filter((d) =>
+        d.name.toLowerCase().includes(valore.toLowerCase())
+    );
+
+    const bioFiltrata = user.filter(d => d.biography.toLowerCase().includes(valore.toLocaleLowerCase()))
+
+
     return (
         <div className="container">
             <h1>Lista Politici</h1>
-            {user.map(cur => {
-                return (
+            <div>
+                <input  className="input-search" type="text" onChange={handleChange} value={valore} placeholder="Cerca per nome o biografia..." />
+                
+                {filtrati.length > 0 
+                ? filtrati.map(cur => (
                     <div className="card">
-                       
-                       <h2>{cur.name}</h2>
-                       <div className="img-container">
+
+                    <h2>{cur.name}</h2>
+                    <div className="img-container">
                         <img src={cur.image} alt="" />
-                       </div>
-                       <p>{cur.position}</p>
-                       <p>{cur.biography}</p>
                     </div>
-                )
-            })}
+                    <p>{cur.position}</p>
+                    <p>{cur.biography}</p>
+                </div>
+            )) 
+                : bioFiltrata.map(cur => (
+                    <div className="card">
+
+                    <h2>{cur.name}</h2>
+                    <div className="img-container">
+                        <img src={cur.image} alt="" />
+                    </div>
+                    <p>{cur.position}</p>
+                    <p>{cur.biography}</p>
+                </div>
+                ))}
+            </div>
+            
         </div>
     )
 }
